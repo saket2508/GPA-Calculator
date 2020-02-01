f1a90fbeaeaf115a6f14fa9675ca84595ad5ccca
Saket S Iyer <52590360+saket2508@users.noreply.github.com>
2020-02-01 11:20:59 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,11 +1,11 @@
 import sqlite3
 from app import StudentGrade
 
-conn= sqlite3.connect('employees.db')
+conn= sqlite3.connect('student.db')
 
 c= conn.cursor()
 
-# c.execute("""CREATE TABLE employees(
+# c.execute("""CREATE TABLE students(
 #            name text,
 #            div text,
 #            regno text,
@@ -15,11 +15,11 @@ c= conn.cursor()
 
 #st1= StudentGrade()
 #gpa= st1.calculateGPA()
-#c.execute("INSERT INTO employees VALUES(?,?,?,?,?)",(st1.name,st1.div,st1.regNo,st1.address,gpa))
+#c.execute("INSERT INTO students VALUES(?,?,?,?,?)",(st1.name,st1.div,st1.regNo,st1.address,gpa))
 
 #conn.commit()
 
-c.execute("SELECT * FROM employees")
+c.execute("SELECT * FROM students")
 print(c.fetchall())
 
 conn.close()
