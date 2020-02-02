b879a7e6dddc2905c0843a4c307a6ae44b3d422c
Saket S Iyer <52590360+saket2508@users.noreply.github.com>
2020-02-02 21:48:04 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,26 +1,33 @@
 import sqlite3
+import datetime
 from app import StudentGrade
 
-conn= sqlite3.connect('student.db')
-
+conn= sqlite3.connect('students.db')
 c= conn.cursor()
 
-# c.execute("""CREATE TABLE students(
-#            name text,
-#            div text,
-#            regno text,
-#            address text,
-#            gpa real
-#            )""")
+def createTable():
+    c.execute('CREATE TABLE IF NOT EXISTS studentsdb(date text,name text,address text,div text,gpa real)')
+    n= int(input('enter the no of students'))
+    date= datetime.datetime.now()
+    dt= date.strftime("%d-%b-%Y")
+    for i in range(n):
+        st= StudentGrade()
+        gpa= st.calculateGPA()
+        c.execute("INSERT INTO studentsdb(date,name,address,div,gpa) VALUES(?, ?, ?, ?, ?)",
+                  (dt,st.name,st.address,st.div,gpa))
+        conn.commit()
 
-#st1= StudentGrade()
-#gpa= st1.calculateGPA()
-#c.execute("INSERT INTO students VALUES(?,?,?,?,?)",(st1.name,st1.div,st1.regNo,st1.address,gpa))
 
-#conn.commit()
+def showData():
+    c.execute('SELECT * FROM studentsdb WHERE gpa=10')
+    for row in c.fetchall():
+        print(row[0])
 
-c.execute("SELECT * FROM students")
-print(c.fetchall())
 
-conn.close()
+def main():
+    showData()
+    conn.close()
+if __name__=="__main__":
+    main()
+
 
